a17057d49678a0a297cd79473c90d29aaa86503a
Carol Villareal <carolvillareal16@gmail.com>
2023-05-17 15:26:01 +0800

    second commit coming soon folder

--- js/script.js
+++
@@ -1,8 +0,0 @@
-let nav = document.querySelector(".navbar");
-window.onscroll = function () {
-  if (document.documentElement.scrollTop > 20) {
-    nav.classList.add("header-scrolled");
-  } else {
-    nav.classList.remove("header-scrolled");
-  }
-};
