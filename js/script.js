7bdf54bdd1804d9fdb05728ca18cb3cc0f94fa30
Carol Villareal <carolvillareal16@gmail.com>
2023-06-15 22:53:33 +0800

    modify hire me button

--- js/script.js
+++ js/script.js
@@ -6,3 +6,29 @@ window.onscroll = function () {
     nav.classList.remove("header-scrolled");
   }
 };
+
+let popup = document.getElementById("popup");
+let okbutton = document.getElementById("okbtn");
+
+const Hirebtn = document.getElementById("hire_me");
+if (Hirebtn) {
+  Hirebtn.addEventListener("click", () => {
+    popup.classList.add("open-popup");
+  });
+}
+if (okbutton) {
+  okbutton.addEventListener("click", () => {
+    popup.classList.remove("open-popup");
+  });
+}
+
+//CONTACT PAGE
+// start console
+let email = document.forms["form"]["email"];
+let password = document.forms["form"]["password"];
+
+let emailError = document.querySelector("#email_error");
+let passwordError = document.querySelector("#password_error");
+// end of console
+
+// paganahin ang email
